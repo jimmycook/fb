81209ce7ec419964d5b9e59b715d0806beedaddd
jimmy <jimmy.cook.1993@gmail.com>
2017-04-07 15:10:28 +0100

    moved to root directory

--- webpack.config.js
+++ webpack.config.js
@@ -26,7 +26,7 @@ module.exports = {
     }
   },
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: path.join(__dirname),
     compress: true,
     port: 8080,
     watchOptions: {
